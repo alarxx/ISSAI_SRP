2d2fc734aa38349695de95ff36e94efc4f2f8992
Alar Akilbekov <alar.akilbekov@gmail.com>
2025-07-25 15:15:15 +0600

    build: init script

---
+++ init.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# chmod +x init.sh
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+touch .env
+# We can use GPUs:{2,3,4,5}
+# Append to file: `echo "CUDA_VISIBLE_DEVICES=3,4" >> .env`
+# Overwrite the file:
+echo "CUDA_VISIBLE_DEVICES=3,4" > .env
