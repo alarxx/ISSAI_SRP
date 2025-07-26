e8bcbe2db66bb2d40cd16fd2b3d19ef48f290d78
alarxx <alar.akilbekov@gmail.com>
2025-07-27 04:04:47 +0500

    feat: add Model Parallel

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
+"""
+SPDX-License-Identifier: MPL-2.0
+--------------------------------
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,
+You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This file is part of the ISSAI Summer Research Project.
+
+Most of the code is taken from tutorial below:
+https://docs.pytorch.org/tutorials/intermediate/ddp_tutorial.html
+
+Provided “as is”, without warranty of any kind.
+
+Copyright © 2025 Alar Akilbekov. All rights reserved.
+
+Third party copyrights are property of their respective owners.
+"""
+
 import os
 import sys
 import tempfile
@@ -60,23 +79,46 @@ def cleanup():
 
 
 class ToyModel(nn.Module):
-    def __init__(self):
+    def __init__(self, dev0, dev1):
         super(ToyModel, self).__init__()
-        self.net1 = nn.Linear(10, 10)
-        self.relu = nn.ReLU()
-        self.net2 = nn.Linear(10, 5)
+        self.dev0 = dev0
+        self.dev1 = dev1
+        self.net1 = torch.nn.Linear(10, 10).to(dev0)
+        self.relu = torch.nn.ReLU()
+        self.net2 = torch.nn.Linear(10, 5).to(dev1)
+        print(f"[forward] net1 on {self.net1.weight.device}")
+        print(f"[forward] net2 on {self.net2.weight.device}")
 
     def forward(self, x):
-        return self.net2(self.relu(self.net1(x)))
+        x = x.to(self.dev0)
+        # print(f"[forward] input is on {x.device}")
+        x = self.net1(x)
+        x = self.relu(x)
+        x = x.to(self.dev1)
+        x = self.net2(x)
+        return x
 
 
-def demo_basic(rank, world_size):
+def demo(rank, world_size):
     print(f"Running basic DDP example on rank {rank}.")
     setup(rank, world_size)
 
+    # setup mp_model and devices for this process
+    dev0 = rank * 2
+    dev1 = rank * 2 + 1
+    print(f"[rank{rank}] uses dev0={dev0}, dev1={dev1}")
+
     # create model and move it to GPU with id rank
-    model = ToyModel().to(rank)
-    ddp_model = DDP(model, device_ids=[rank]) # synchronizes randomly initialized parameters
+    # model = ToyModel(dev0, dev1).to(rank)
+    model = ToyModel(dev0, dev1) # не указываем device при Model Parallel
+
+    # DDP synchronizes randomly initialized parameters
+    # DDP вешает hook на параметры для синхронизации, когда .grad будут готовы
+    # ddp_model = DDP(model, device_ids=[rank]) # device_ids перемещает input на нужный device
+    ddp_model = DDP(model) # не указываем device_ids при Model Parallel
+
+    for name, p in ddp_model.module.named_parameters():
+        print(f"[rank{rank}] param {name} is on {p.device}")
 
     CHECKPOINT_PATH = tempfile.gettempdir() + "/model.checkpoint"
     if rank == 0:
@@ -85,21 +127,33 @@ def demo_basic(rank, world_size):
         torch.save(ddp_model.state_dict(), CHECKPOINT_PATH)
 
     # Use a barrier() to make sure that process 1 loads the model after process 0 saves it.
-    dist.barrier(device_ids=[rank]) # точка синхронизации, задача в очереди GPU по типу all-reduce блокирует thread-GPU
-    # configure map_location properly
-    map_location = {f'cuda:{0}': f'cuda:{rank}'} # map parameters to corresponding process-GPU-device
+    dist.barrier(device_ids=[dev0, dev1]) # точка синхронизации, задача в очереди GPU по типу all-reduce блокирует thread-GPU
+    # Configure map_location properly
+    # map_location = {f'cuda:{0}': f'cuda:{rank}'} # map parameters to corresponding process-GPU-device
+    map_location = {
+        'cuda:0': f'cuda:{rank * 2}',
+        'cuda:1': f'cuda:{rank * 2 + 1}'
+    }
     ddp_model.load_state_dict(
         torch.load(CHECKPOINT_PATH, map_location=map_location, weights_only=True)
     )
 
+    """
+        Я так понял все градиенты вычисляются и потом синхронизируются.
+        А на каком девайсе делается step?
+        Мы синхронизируем градиенты и потом каждый процесс сам делает step, потому что иначе потом пришлось бы синхронизировать модель
+    """
     loss_fn = nn.MSELoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 
-    optimizer.zero_grad()
-    outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(rank)
-    loss_fn(outputs, labels).backward()
-    optimizer.step()
+    for i in range(10000):
+        print(f"epoch {i}")
+        optimizer.zero_grad()
+        outputs = ddp_model(torch.randn(20, 10))
+        labels = torch.randn(20, 5).to(dev1)
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
     # Not necessary to use a dist.barrier() to guard the file deletion below
     if rank == 0:
@@ -114,10 +168,11 @@ if __name__ == "__main__":
     if(not devices.print_available_devices()):
         exit()
 
-    # https://docs.pytorch.org/tutorials/intermediate/ddp_tutorial.html
-    world_size = torch.cuda.device_count()
-    assert world_size >= 2, f"Requires at least 2 GPUs to run, but got {world_size}"
-    mp.spawn(demo_basic,
+    n_gpus = torch.cuda.device_count()
+    assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {world_size}"
+
+    world_size = n_gpus//2 # works on odd number of GPUs
+    mp.spawn(demo,
              args=(world_size,),
              nprocs=world_size,
              join=True)
