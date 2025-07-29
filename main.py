a8b7fc47af61dbe8ba1ce5cdebec91dbba8bd34d
Alar Akilbekov <alar.akilbekov@gmail.com>
2025-07-29 15:24:36 +0600

    refactor: test.py -> main.py

--- main.py
+++ main.py
@@ -1,213 +1,315 @@
-import torch
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
+"""
+SPDX-License-Identifier: MPL-2.0
+--------------------------------
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,
+You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# NEW imports:
+This file is part of the ISSAI Summer Research Project.
+
+Most of the code is taken from tutorial below:
+https://docs.pytorch.org/tutorials/intermediate/ddp_tutorial.html
+
+Provided “as is”, without warranty of any kind.
+
+Copyright © 2025 Alar Akilbekov. All rights reserved.
+
+Third party copyrights are property of their respective owners.
+"""
+
 import os
 import platform
+import sys
+import tempfile
+import datetime
+import dotenv 
+# os.environ["CUDA_VISIBLE_DEVICES"] = "3,4"
+dotenv.load_dotenv()  # загрузит переменные из .env
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+
+# utils
+from utils.data.CustomImageDataset import CustomImageDataset
+from utils import devices
+from utils.visualizer import visualize_image_dataset, plot4
+from utils import saver
+# from utils.trainer import Trainer
+from utils.model.MLP import MLP
+# from utils import MyTrainDataset
+
+import torch.multiprocessing as mp
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group, destroy_process_group
+import torch.distributed as dist
 
+# ---
 
-# NEW: function to initialize a distributed process group (1 process / GPU)
-# this allows communication among processes
-def ddp_setup(rank, world_size):
+CHANNELS, HEIGHT, WIDTH = 3, 28, 28
+BATCH_SIZE = 64
+
+# ---
+
+# On Windows platform, the torch.distributed package only
+# supports Gloo backend, FileStore and TcpStore.
+# For FileStore, set init_method parameter in init_process_group
+# to a local file. Example as follow:
+# init_method="file:///f:/libtmp/some_file"
+# dist.init_process_group(
+#    "gloo",
+#    rank=rank,
+#    init_method=init_method,
+#    world_size=world_size)
+# For TcpStore, same way as on Linux.
+
+def setup(rank: int, world_size: int):
     """
-    Arguments:
-        rank: a unique process ID
-        world_size: total number of processes in the group
+    Args:
+        rank: Unique identifier of each process
+        world_size: Total number of processes
     """
-    # Only set MASTER_ADDR and MASTER_PORT if not already defined by torchrun
-    if "MASTER_ADDR" not in os.environ:
-        os.environ["MASTER_ADDR"] = "localhost"
-    if "MASTER_PORT" not in os.environ:
-        os.environ["MASTER_PORT"] = "12345"
-
+    os.environ['MASTER_ADDR'] = 'localhost' # on cluster of machines should be explicit IP 
+    os.environ['MASTER_PORT'] = '12480' # ports range 2^16: [0, 65535], better choose port>1024
+    # https://docs.pytorch.org/tutorials/beginner/ddp_series_multigpu.html#constructing-the-process-group
+    torch.cuda.set_device(rank) # sets the default GPU for each process
     # initialize process group
     if platform.system() == "Windows":
         # Disable libuv because PyTorch for Windows isn't built with support
         os.environ["USE_LIBUV"] = "0"
         # Windows users may have to use "gloo" instead of "nccl" as backend
         # gloo: Facebook Collective Communication Library
-        init_process_group(backend="gloo", rank=rank, world_size=world_size)
+        dist.init_process_group(backend="gloo", rank=rank, world_size=world_size)
     else:
         # nccl: NVIDIA Collective Communication Library
-        init_process_group(backend="nccl", rank=rank, world_size=world_size)
+        # initialize the process group
+        dist.init_process_group(
+            backend="nccl",
+            rank=rank,
+            world_size=world_size,
+            timeout=datetime.timedelta(minutes=10) # Default is 10 minutes for NCCL and 30 minutes for other backends
+            )
 
-    torch.cuda.set_device(rank)
-
-
-class ToyDataset(Dataset):
-    def __init__(self, X, y):
-        self.features = X
-        self.labels = y
-
-    def __getitem__(self, index):
-        one_x = self.features[index]
-        one_y = self.labels[index]
-        return one_x, one_y
-
-    def __len__(self):
-        return self.labels.shape[0]
-
-
-class NeuralNetwork(torch.nn.Module):
-    def __init__(self, num_inputs, num_outputs):
-        super().__init__()
-
-        self.layers = torch.nn.Sequential(
-            # 1st hidden layer
-            torch.nn.Linear(num_inputs, 30),
-            torch.nn.ReLU(),
-
-            # 2nd hidden layer
-            torch.nn.Linear(30, 20),
-            torch.nn.ReLU(),
-
-            # output layer
-            torch.nn.Linear(20, num_outputs),
-        )
-
-    def forward(self, x):
-        logits = self.layers(x)
-        return logits
+def cleanup():
+    dist.destroy_process_group()
 
 
 def prepare_dataset():
-    X_train = torch.tensor([
-        [-1.2, 3.1],
-        [-0.9, 2.9],
-        [-0.5, 2.6],
-        [2.3, -1.1],
-        [2.7, -1.5]
+    transform = transforms.Compose([
+        transforms.Resize((HEIGHT, WIDTH)),  # например
+        transforms.ToTensor(), # tensor(C, H, W) in RGB
     ])
-    y_train = torch.tensor([0, 0, 0, 1, 1])
 
-    X_test = torch.tensor([
-        [-0.8, 2.8],
-        [2.6, -1.6],
-    ])
-    y_test = torch.tensor([0, 1])
+    dataset = CustomImageDataset(root_dir="dataset/CMNIST", transform=transform)
 
-    # Uncomment these lines to increase the dataset size to run this script on up to 8 GPUs:
-    # factor = 4
-    # X_train = torch.cat([X_train + torch.randn_like(X_train) * 0.1 for _ in range(factor)])
-    # y_train = y_train.repeat(factor)
-    # X_test = torch.cat([X_test + torch.randn_like(X_test) * 0.1 for _ in range(factor)])
-    # y_test = y_test.repeat(factor)
+    # Как разделить на Train, Val, Test
+    # validation_data = K-Fold Cross Validation on training_data
+    training_data, test_data = dataset.train_test_split(test_size=0.25, random_state=42)
+    
+    # param dir_path, не храни пути, возвращай изображения по индексам восстанавливая путь к файлу
 
-    train_ds = ToyDataset(X_train, y_train)
-    test_ds = ToyDataset(X_test, y_test)
-
-    train_loader = DataLoader(
-        dataset=train_ds,
-        batch_size=2,
+    # Create data loaders.
+    # Data Loader wraps an iterable over dataset
+    train_dataloader = DataLoader(
+        dataset=training_data, 
+        batch_size=BATCH_SIZE, 
         shuffle=False,  # NEW: False because of DistributedSampler below
+        # Но почему shuffe нужно отключать, как работает DistributedSampler, как он обеспечивает non overlapping batches?
+        # if shuffle and sampler is not None: throw error - лучше ошибка чем баг? Или происходит просто двойной shuffle?
+        # DistributedSampler работает без синхронизации между процессами, использует rank и world_size из global state
         pin_memory=True,
         drop_last=True,
-        # NEW: chunk batches across GPUs without overlapping samples:
-        sampler=DistributedSampler(train_ds)  # NEW
+        sampler=DistributedSampler(training_data)  # NEW chunk batches across GPUs without overlapping samples
     )
-    test_loader = DataLoader(
-        dataset=test_ds,
-        batch_size=2,
-        shuffle=False,
+    
+    test_dataloader = DataLoader(
+        dataset=test_data, 
+        batch_size=BATCH_SIZE, 
+        shuffle=False
     )
-    return train_loader, test_loader
+
+    return train_dataloader, test_dataloader, dataset.classes
 
 
-# NEW: wrapper
-def main(rank, world_size, num_epochs):
+def ddp_train_epoch(rank, world_size, ddp_model, loss_fn, dataloader, optimizer, epoch):
+    device = rank
+    dataloader.sampler.set_epoch(epoch)
+    ddp_model.train()
+    size = len(dataloader.dataset) # количество всех примеров в датасете
+    num_batches = len(dataloader) * world_size # количество батчей, i.e. dataset/batch_size
+    train_loss, n_correct = torch.tensor(0., device=device), torch.tensor(0., device=device)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
 
-    ddp_setup(rank, world_size)  # NEW: initialize process groups
+        # Compute prediction error
+        pred = ddp_model(X)
+        loss = loss_fn(pred, y)
 
-    train_loader, test_loader = prepare_dataset()
-    model = NeuralNetwork(num_inputs=2, num_outputs=2)
-    model.to(rank)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-    model = DDP(model, device_ids=[rank])  # NEW: wrap model with DDP
-    # the core model is now accessible as model.module
+        train_loss_ = loss 
+        n_correct_  = (pred.argmax(1) == y).type(torch.float).sum()
+        # print("loss:", loss.dtype, "; correct:", correct.dtype)
 
-    for epoch in range(num_epochs):
-        # NEW: Set sampler to ensure each epoch has a different shuffle order
-        train_loader.sampler.set_epoch(epoch)
+        # Synchronize current loss and number of correctly predicted
+        dist.all_reduce(train_loss_,    op=dist.ReduceOp.SUM)
+        dist.all_reduce(n_correct_,     op=dist.ReduceOp.SUM)
 
-        model.train()
-        for features, labels in train_loader:
+        train_loss  += train_loss_
+        n_correct   += n_correct_
 
-            features, labels = features.to(rank), labels.to(rank)  # New: use rank
-            logits = model(features)
-            loss = F.cross_entropy(logits, labels)  # Loss function
+        if rank == 0 and batch % 100 == 0:
+            loss, current = train_loss_.item(), (batch + 1) * dataloader.batch_size 
+            print(f"[GPU{rank}] loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    train_loss  /= num_batches  # средний loss на эпохе
+    n_correct   /= size         # accuracy
 
-            # LOGGING
-            print(f"[GPU{rank}] Epoch: {epoch+1:03d}/{num_epochs:03d}"
-                  f" | Batchsize {labels.shape[0]:03d}"
-                  f" | Train/Val Loss: {loss:.2f}")
+    return train_loss.item(), n_correct.item() # each process translates to CPU but once in epoch
 
-    model.eval()
 
-    try:
-        train_acc = compute_accuracy(model, train_loader, device=rank)
-        print(f"[GPU{rank}] Training accuracy", train_acc)
-        test_acc = compute_accuracy(model, test_loader, device=rank)
-        print(f"[GPU{rank}] Test accuracy", test_acc)
+def validate(rank, ddp_model, loss_fn, dataloader):
+    device = rank
+    ddp_model.eval()
+    size = len(dataloader.dataset) # количество всех примеров в датасете
+    num_batches = len(dataloader) # количество батчей, i.e. dataset/batch_size
+    test_loss, n_correct = torch.tensor(0., device=device), torch.tensor(0., device=device)
+    with torch.no_grad(): # temporarily changing global state - torch.is_grad_enabled()
+        for batch, (X, y) in enumerate(dataloader):
+            X, y = X.to(device), y.to(device)
+            pred = ddp_model(X)
+            test_loss += loss_fn(pred, y)
+            n_correct += (pred.argmax(1) == y).type(torch.float).sum()
 
-    ####################################################
-    # NEW:
-    except ZeroDivisionError as e:
-        raise ZeroDivisionError(
-            f"{e}\n\nThis script is designed for 2 GPUs. You can run it as:\n"
-            "torchrun --nproc_per_node=2 DDP-script-torchrun.py\n"
-            f"Or, to run it on {torch.cuda.device_count()} GPUs, uncomment the code on lines 103 to 107."
+        test_loss /= num_batches
+        n_correct /= size
+    print(f"Test Error: \n Accuracy: {(100*n_correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return test_loss.item(), n_correct.item()
+
+
+def demo(rank, world_size):
+    print(f"Running basic DDP example on rank {rank}.")
+    setup(rank, world_size)
+    # rank and world_size are global variables
+    print("dist.get_rank():", dist.get_rank())
+    print("dist.get_world_size():", dist.get_world_size())
+
+    train_loader, test_loader, classes = prepare_dataset()
+    visualize_image_dataset(train_loader.dataset, classes)
+
+    model = MLP(n_in=CHANNELS*HEIGHT*WIDTH, n_out=len(classes)).to(rank)
+
+    # DDP synchronizes randomly initialized parameters
+    # DDP вешает hook на параметры для синхронизации, когда .grad будут готовы
+    ddp_model = DDP(model, device_ids=[rank]) # device_ids перемещает input на нужный device
+    # ddp_model = DDP(model) # не указываем device_ids при Model Parallel
+
+    # --- Saving START ---
+
+    for name, p in ddp_model.module.named_parameters():
+        print(f"[rank{rank}] param {name} is on {p.device}")
+
+    CHECKPOINT_PATH = tempfile.gettempdir() + "/model.checkpoint"
+    if rank == 0:
+        # Сохраняя state_dict сохраняются и device-ы на котором находились тензоры, here cuda:0
+        # saver.print_state_dict(model)
+        torch.save(ddp_model.state_dict(), CHECKPOINT_PATH)
+        # What if we call dist.barrier() here, so for rank0 will be twice, will we just stuck on rank0 process?
+
+    # Use a barrier() to make sure that process 1 loads the model after process 0 saves it.
+    # A process is blocked by a barrier until all processes have encountered a barrier, upon which the barrier is lifted for all.
+    # IMO, in our case no need to synchronize by device_ids, но было бы нужно если бы на одном процессе 2 потока использовали разные видеокарты асинхронно
+    dist.barrier(device_ids=[rank]) # точка синхронизации, задача в очереди GPU по типу all-reduce блокирует thread-GPU
+    # Configure map_location properly
+    map_location = {f'cuda:{0}': f'cuda:{rank}'} # map parameters to corresponding process-GPU-device
+    # map_location = {
+    #     'cuda:0': f'cuda:{rank * 2}',
+    #     'cuda:1': f'cuda:{rank * 2 + 1}'
+    # }
+    ddp_model.load_state_dict(
+        torch.load(CHECKPOINT_PATH, map_location=map_location, weights_only=True)
+    )
+
+    # --- Saving END ---
+
+    """
+        Я так понял все градиенты вычисляются и потом синхронизируются.
+        А на каком девайсе делается step?
+        Мы синхронизируем градиенты и потом каждый процесс сам делает step, потому что иначе потом пришлось бы синхронизировать модель
+    """
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(ddp_model.parameters(), lr=0.001)
+
+    EPOCHS = 5
+    train_losses, train_accuracies = [], []
+    val_losses, val_accuracies = [], []
+    for epoch in range(EPOCHS):
+        print(f"epoch {epoch}")
+
+        train_loss, train_acc = ddp_train_epoch(
+            rank, world_size,
+            ddp_model,
+            loss_fn,
+            train_loader,
+            optimizer,
+            epoch
         )
-    ####################################################
 
-    destroy_process_group()  # NEW: cleanly exit distributed mode
+        if rank == 0:
+            print("Hell")
+            train_losses.append(train_loss)
+            train_accuracies.append(train_acc)
+            
+            val_loss, val_acc = validate(
+                rank,
+                ddp_model,
+                loss_fn,
+                test_loader
+            )
+            val_losses.append(val_loss)
+            val_accuracies.append(val_acc)
+
+        dist.barrier(device_ids=[rank])
+
+    # Not necessary to use a dist.barrier() to guard the file deletion below
+    if rank == 0:
+        os.remove(CHECKPOINT_PATH)
+
+    cleanup()
+    print(f"Finished running basic DDP example on rank {rank}.")
 
 
-def compute_accuracy(model, dataloader, device):
-    model = model.eval()
-    correct = 0.0
-    total_examples = 0
-
-    for idx, (features, labels) in enumerate(dataloader):
-        features, labels = features.to(device), labels.to(device)
-
-        with torch.no_grad():
-            logits = model(features)
-        predictions = torch.argmax(logits, dim=1)
-        compare = labels == predictions
-        correct += torch.sum(compare)
-        total_examples += len(compare)
-    return (correct / total_examples).item()
+def handler(rank, world_size):
+    try:
+        demo(rank, world_size)
+    except Exception as e:
+        print(f"Exception on [rank{rank}]:", e)   
+    finally:
+        dist.barrier()  # чтобы не убегал никто раньше
+        cleanup()
+        print(f"Finished running basic DDP example on rank {rank}.")
 
 
+# В дочерних процессах mp.spawn __name__ != "__main__"
 if __name__ == "__main__":
-    # # NEW: Use environment variables set by torchrun if available, otherwise default to single-process.
-    # if "WORLD_SIZE" in os.environ:
-    #     world_size = int(os.environ["WORLD_SIZE"])
-    # else:
-    #     world_size = 1
 
-    # if "LOCAL_RANK" in os.environ:
-    #     rank = int(os.environ["LOCAL_RANK"])
-    # elif "RANK" in os.environ:
-    #     rank = int(os.environ["RANK"])
-    # else:
-    #     rank = 0
+    print("PyTorch version:", torch.__version__)
+    if(not devices.print_available_devices()):
+        exit()
 
-    # # Only print on rank 0 to avoid duplicate prints from each GPU process
-    # if rank == 0:
-    #     print("PyTorch version:", torch.__version__)
-    #     print("CUDA available:", torch.cuda.is_available())
-    #     print("Number of GPUs available:", torch.cuda.device_count())
+    n_gpus = torch.cuda.device_count()
+    assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
 
-    # torch.manual_seed(123)
-    # num_epochs = 3
-    # main(rank, world_size, num_epochs)
+    # world_size = n_gpus//2 # works on odd number of GPUs
+    world_size = n_gpus
+    mp.spawn(handler,
+             args=(world_size,),
+             nprocs=world_size,
+             join=True)
